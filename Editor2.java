f2692e5c6134ca4f0a06fd15ae35708b92fde263
הילה מעברי <hila.mabari@post.runi.ac.il>
2024-12-20 13:39:51 +0200

    Done

--- Editor2.java
+++ Editor2.java
@@ -10,7 +10,23 @@ import java.awt.Color;
 public class Editor2 {
 
 	public static void main (String[] args){
-		//// Replace this comment with your code.
-		//// This function is similar to the main function of Editor1.java			
-	}
+        if (args.length != 3) {
+            System.out.println("Usage: java Editor2 <filename> <width> <height>");
+            return;
+        }
+        String fileName = args[0];
+        int newWidth = Integer.parseInt(args[1]);
+        int newHeight = Integer.parseInt(args[2]);
+        Color[][] imageIn = Runigram.read(fileName);
+        if (imageIn == null) {
+            System.out.println("Error: Unable to read the image file.");
+            return;
+        }
+        Color[][] imageOut = Runigram.scaled(imageIn, newWidth, newHeight);
+        Runigram.setCanvas(imageIn);
+        Runigram.display(imageIn);
+        StdDraw.pause(3000);
+        Runigram.setCanvas(imageOut);
+        Runigram.display(imageOut);
+    }
 }
